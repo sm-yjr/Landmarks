00101af6f005fd4c02347efc1360e0484be9a0a1
yjr <245837941@qq.com>
2022-06-03 10:54:58 +0800

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by 杨佳瑞 on 2022/6/3.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
